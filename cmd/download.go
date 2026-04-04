fab2afd3d9b9ff7b35328f3798ddd7bb19d340f9
Nelo-T. Wallus <red.brush9525@fastmail.com>
2026-04-04 20:12:51 +0200

    Disable gocritic for 0x770
    Signed-off-by: Nelo-T. Wallus <red.brush9525@fastmail.com>

--- cmd/download.go
+++ cmd/download.go
@@ -67,7 +67,7 @@ func Download(ctx context.Context, args []string) error {
 		return err
 	}
 
-	if err := os.Chmod(*fOut, 0x770); err != nil {
+	if err := os.Chmod(*fOut, 0x770); err != nil { //nolint:gocritic // This is to mark the file executable, it has to be >0660
 		return err
 	}
 
