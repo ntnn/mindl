84990422f4327c1e167d8f49789d9dacf7475eb8
Nelo-T. Wallus <red.brush9525@fastmail.com>
2026-04-04 17:24:28 +0200

    Add templating
    Signed-off-by: Nelo-T. Wallus <red.brush9525@fastmail.com>

--- cmd/debug.go
+++ cmd/debug.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
 
 	"github.com/ntnn/mindl/pkg/mindl"
 )
@@ -17,6 +18,8 @@ func Debug(ctx context.Context, args []string) error {
 	switch args[0] {
 	case "template-data":
 		return DebugTemplateData(ctx, os.Stdout)
+	case "template":
+		return DebugTemplate(ctx, os.Stdout, strings.Join(args[1:], " "))
 	default:
 		return fmt.Errorf("unknown command: %q", args[0])
 	}
@@ -28,4 +31,15 @@ func DebugTemplateData(ctx context.Context, out io.Writer) error {
 	encoder := json.NewEncoder(out)
 	encoder.SetIndent("", "  ")
 	return encoder.Encode(td)
+}
+
+func DebugTemplate(ctx context.Context, out io.Writer, text string) error {
+	td := mindl.NewTemplateData()
+	td.Version = "<VERSION>"
+	result, err := mindl.Template(text, td)
+	if err != nil {
+		return err
+	}
+	_, err = fmt.Fprintln(out, result)
+	return err
 }
--- pkg/mindl/template.go
+++ pkg/mindl/template.go
@@ -1,6 +1,12 @@
 package mindl
 
-import "runtime"
+import (
+	"bytes"
+	"fmt"
+	"runtime"
+	"strings"
+	"text/template"
+)
 
 type TemplateData struct {
 	Version string `json:"version"`
@@ -13,4 +19,24 @@ func NewTemplateData() *TemplateData {
 	td.OS = runtime.GOOS
 	td.Arch = runtime.GOARCH
 	return td
+}
+
+var FuncMap = map[string]any{
+	"title": strings.Title,
+}
+
+func Template(text string, data *TemplateData) (string, error) {
+	t := template.New("").Funcs(FuncMap)
+
+	t, err := t.Parse(text)
+	if err != nil {
+		return "", fmt.Errorf("error parsing template: %w", err)
+	}
+
+	b := &bytes.Buffer{}
+	if err := t.Execute(b, data); err != nil {
+		return "", fmt.Errorf("error executing template: %w", err)
+	}
+
+	return b.String(), nil
 }
