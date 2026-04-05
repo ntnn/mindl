21614bd55ead79879f22e24757d2404fe0cb0905
Nelo-T. Wallus <red.brush9525@fastmail.com>
2026-04-06 00:47:19 +0200

    Use correct bzip2 suffix
    Signed-off-by: Nelo-T. Wallus <red.brush9525@fastmail.com>

--- pkg/mindl/archive.go
+++ pkg/mindl/archive.go
@@ -65,7 +65,7 @@ func Untar(archive, target, output string) error {
 			return fmt.Errorf("error adding gzip reader: %w", err)
 		}
 		f = gzreader
-	case strings.HasSuffix(archive, ".bz"):
+	case strings.HasSuffix(archive, ".bz2"):
 		f = bzip2.NewReader(raw)
 	}
 
